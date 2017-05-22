5d1ab72cce47ddade55ae93fc6838e7879bd86ab
Travis Gesslein <travis.gesslein@pitforest.de>
2017-05-22 15:32:32 +0200

    changed picam creation parameters to take image instantly, small bug fixes

--- client/index.js
+++ client/index.js
@@ -2,12 +2,11 @@
 var common = require("../common/common.js"); //code that is common to server and client
 var fs = require('fs');
 var raspicam = require('raspicam');
+var glfw = require('node-glfw-bindings');
 
 //read server address from config
 var serverUrl = config.get('server');
 var imageFolderName = config.get('imageDir');
-
-
 
 var io = require('socket.io-client')(serverUrl); //options are ommited, server tries connection and reconnection in intervals automatically
 
@@ -18,7 +17,6 @@ io.on("connect", function (socket)
 {
 });
 
-
 //server tells me to take image!
 io.on(common.EVENT_TYPES.TAKE_IMAGE, function (data) {
     takeImage(function (imageFilename) {
@@ -28,18 +26,67 @@ io.on(common.EVENT_TYPES.TAKE_IMAGE, function (data) {
     });
 });
 
+
+//sends an error string to the server
+function serverError(string) {
+    io.emit(common.EVENT_TYPES.ERROR, { message: string });
+}
+
 //takes an image using the pi cam and calls the given callback on success
 //callback arguments are (imagefilename)
 function takeImage(callback) {
-    //callback(imageFolderName + "/test.jpg");
+    callback(imageFolderName + "/test.jpg");
+	return;
     var camera = new raspicam({
         mode: "photo",
-        output: imageFolderName + "/%d.jpg"
+        output: imageFolderName + "/%d.jpg",
+		timeout: 1,
+		nopreview: true
     });
-    camera.on("read", function (error, filename) {
-        callback(filename);
+    camera.on("read", function (thisArg, error, filename) {
+        var filenameStr = imageFolderName + "/" + filename;
+			if(filenameStr.charAt(filenameStr.length-1) !== "~")
+				callback(filenameStr);
     });
 
     camera.start();
-    camera.stop();
 }
+
+//creates window for beamer output
+function createWindow() {
+    if (!glfw.glfwInit()) {
+        console.log("failed initializing glfw");
+        serverError("failed initializing glfw");
+    }
+
+    //using node-glfw
+
+    var monitor = glfw.glfwGetPrimaryMonitor();
+    var mode = glfw.glfwGetVideoMode(monitor);
+    console.log(glfw.glfwGetVersion());
+    glfw.glfwWindowHint(glfw.GLFW_RED_BITS, mode.redBits);
+    glfw.glfwWindowHint(glfw.GLFW_GREEN_BITS, mode.greenBits);
+    glfw.glfwWindowHint(glfw.GLFW_BLUE_BITS, mode.blueBits);
+    glfw.glfwWindowHint(glfw.GLFW_REFRESH_RATE, mode.refreshRate);
+    glfw.glfwWindowHint(glfw.GLFW_DECORATED, 0);
+    var window = glfw.glfwCreateWindow(mode.width, mode.height, "window", monitor, null);
+    return window;
+}
+
+function update(window) {
+    glfw.glfwPollEvents();
+    glfw.glfwSwapBuffers(window);
+}
+
+var isProjectorClient = config.get('isProjectorClient'); //tells us if this client is connected to a beamer, and should draw the pattern / black image
+
+if (isProjectorClient) { //projector clients need to create a window to output images to 
+    var window = createWindow();
+
+    while (!glfw.glfwWindowShouldClose(window)) {
+        update(window);
+    }
+}
+
+glfw.glfwTerminate();
+
