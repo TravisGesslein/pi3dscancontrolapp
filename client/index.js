83d756dffa6d3a7ad914cdc5a8f5091c7ac1687d
Travis Gesslein <travis.gesslein@pitforest.de>
2017-05-29 19:29:30 +0200

    experimental changes

--- client/index.js
+++ client/index.js
@@ -2,7 +2,10 @@
 var common = require("../common/common.js"); //code that is common to server and client
 var fs = require('fs');
 var raspicam = require('raspicam');
-var glfw = require('node-glfw-bindings');
+var sdl = require('node-sdl2');
+
+//var glfw = require('node-glfw-bindings'); 
+//var gl = require('gl');
 
 //read server address from config
 var serverUrl = config.get('server');
@@ -68,8 +71,9 @@ function createWindow() {
     glfw.glfwWindowHint(glfw.GLFW_GREEN_BITS, mode.greenBits);
     glfw.glfwWindowHint(glfw.GLFW_BLUE_BITS, mode.blueBits);
     glfw.glfwWindowHint(glfw.GLFW_REFRESH_RATE, mode.refreshRate);
-    glfw.glfwWindowHint(glfw.GLFW_DECORATED, 0);
-    var window = glfw.glfwCreateWindow(mode.width, mode.height, "window", monitor, null);
+    //glfw.glfwWindowHint(glfw.GLFW_DECORATED, 0);
+    //var window = glfw.glfwCreateWindow(mode.width, mode.height, "window", monitor, null);
+    var window = glfw.glfwCreateWindow(800,600, "window", monitor, null);
     return window;
 }
 
@@ -81,12 +85,30 @@ function update(window) {
 var isProjectorClient = config.get('isProjectorClient'); //tells us if this client is connected to a beamer, and should draw the pattern / black image
 
 if (isProjectorClient) { //projector clients need to create a window to output images to 
-    var window = createWindow();
+    /*var window = createWindow();
+
+    gl = gl(800, 600, { preserveDrawingBuffer: true });
+    
 
     while (!glfw.glfwWindowShouldClose(window)) {
         update(window);
     }
+
+    glfw.glfwTerminate();
+    */
+
+    const App = sdl.app;
+    const Window = sdl.window;
+
+    let win = new Window
+    win.on('close', () => {
+        App.quit()
+    });
+
+    win.on('change', () => {
+        console.log("hello");
+    });
 }
 
-glfw.glfwTerminate();
+
 
